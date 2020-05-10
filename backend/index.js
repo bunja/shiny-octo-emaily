d3679a4aa8d44e4487a0bb4269d0f823e9998cbb
Larisa Kosareva <kos.lv@yandex.com>
2020-05-10 23:31:49 +0200

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hey: "there"});
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(8080);
