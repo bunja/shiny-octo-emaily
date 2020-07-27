351916f8c1af83c306fe419c0dffce33fb6b0958
Larisa Kosareva <kos.lv@yandex.com>
2020-07-27 10:48:32 +0200

    client side added

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys')
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
+
 //const authRoutes = require('./routes/authRouts'); istead of this
 //authRoutes(app); and that we just can do 
 //require('./routes/authRouts')(app)
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRouts')(app);
 
 
@@ -14,4 +31,6 @@ require('./routes/authRouts')(app);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT);
+app.listen(8080, function() {
+    console.log("port 8080 I'm listening.");
+});
