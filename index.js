7336e3e565142813ba1d1939043aa2aee3a80dc2
Larisa Kosareva <kos.lv@yandex.com>
2020-09-24 15:05:40 +0200

    adding charge, wip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys')
 require('./models/User');
 require('./services/passport');
@@ -13,6 +14,8 @@ const app = express();
 //authRoutes(app); and that we just can do 
 //require('./routes/authRouts')(app)
 
+app.use(bodyParser.json());
+
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,7 +26,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRouts')(app);
-
+require('./routes/billingRouts')(app);
 
 
 
