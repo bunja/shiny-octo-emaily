8d1d7dda07023415c50886c110d52768b3e78631
Larisa Kosareva <kos.lv@yandex.com>
2020-10-12 23:51:07 +0200

    client addButton, dashboard, survey

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,17 @@
+//SurveyNew shows SurveyForm and SurveyReviewForm
+
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+    render() {
+        return (
+            <div> 
+                SurveyNew!
+                <SurveyForm/>
+            </div>
+        );
+    }
+}
+
+export default SurveyNew;
