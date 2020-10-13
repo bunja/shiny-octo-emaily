efcad8ca699dfed6e226884e1642a8e6a377f2a3
Larisa Kosareva <kos.lv@yandex.com>
2020-10-13 15:19:51 +0200

    validateEmail

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field, clearSubmitErrors } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
     {label:'Survey Title', name: 'title'},
@@ -38,11 +39,15 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
+    errors.emails = validateEmails(values.emails || '');
+
     _.each(FIELDS, ({ name}) => {
         if(!values[name]) {
             errors[name] = 'You must provide a value';
         }
     });
+
+    
 
     return errors;
 }
