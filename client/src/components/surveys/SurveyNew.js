b7f0d010681c93ba85bf9fc62396c748772c2f36
Larisa Kosareva <kos.lv@yandex.com>
2020-10-13 23:11:02 +0200

    review form, back button

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,12 +2,28 @@
 
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    state = { showReview: false};
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview
+                        onCancel={() => this.setState({ showFormReview: false})}
+                    />;
+        }
+        return (
+            <SurveyForm 
+                onSurveySubmit={() => this.setState({ showFormReview: true})}
+            />
+        );
+    }
+
     render() {
         return (
             <div> 
-                <SurveyForm/>
+                {this.renderContent()}
             </div>
         );
     }
