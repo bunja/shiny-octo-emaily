e9c975f45639929400e122a6d3bb1637d5e7e35b
Larisa Kosareva <kos.lv@yandex.com>
2020-10-13 23:39:30 +0200

    dumping form fields

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,6 +1,7 @@
 //SurveyNew shows SurveyForm and SurveyReviewForm
 
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -29,4 +30,6 @@ class SurveyNew extends Component {
     }
 }
 
-export default SurveyNew;
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyNew);
